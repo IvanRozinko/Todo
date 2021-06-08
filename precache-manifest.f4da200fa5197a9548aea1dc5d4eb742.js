573ec86074a69a72480f3f839d81de6d2344c6a3
Ivan Rozinko <ivan.rozinko@globallogic.com>
2021-06-08 10:17:35 +0300

    Updates

---
+++ precache-manifest.f4da200fa5197a9548aea1dc5d4eb742.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f7c130adf2acc4124557a4a553eaab",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "43b21f54c25becb5510e",
+    "url": "/Todo/static/css/2.7c6b75c7.chunk.css"
+  },
+  {
+    "revision": "53665e57a9389a5c8ffa",
+    "url": "/Todo/static/css/main.8746d2a8.chunk.css"
+  },
+  {
+    "revision": "43b21f54c25becb5510e",
+    "url": "/Todo/static/js/2.9ba47492.chunk.js"
+  },
+  {
+    "revision": "a2e13987a03a68248676217d21bf5906",
+    "url": "/Todo/static/js/2.9ba47492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53665e57a9389a5c8ffa",
+    "url": "/Todo/static/js/main.dd3d4d75.chunk.js"
+  },
+  {
+    "revision": "162a65c0456d03b31aa2",
+    "url": "/Todo/static/js/runtime-main.e3579041.js"
+  }
+]);
